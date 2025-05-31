880f8f1dd3f08b68aa0004ef810af5d9c69c290b
VaanHovanisian <hovanisianvaan@gmail.com>
2025-05-31 10:23:29 +0400

    apelsin-level-update

--- src/app/api/basket/route.ts
+++ src/app/api/basket/route.ts
@@ -1,9 +1,7 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 import crypto from "crypto";
-import { findOrCreateBasket } from "@/lib/find-or-create-basket";
 import { CreateBasketCardValues } from "@/@types/basket";
-import { updateBasketTotalAmount } from "@/lib/update-basket-total-amount";
 
 export async function GET(req: NextRequest) {
   try {
@@ -37,59 +35,5 @@ export async function GET(req: NextRequest) {
   } catch (error) {
     console.log("GET_CATCH ERROR", error);
     return NextResponse.json({ error: "basket not found" });
-  }
-}
-
-export async function POST(req: NextRequest) {
-  try {
-    let token = req.cookies.get("basketToken")?.value;
-
-    if (!token) {
-      token = crypto.randomUUID();
-    }
-
-    const userBasket = await findOrCreateBasket(token);
-
-    const data = (await req.json()) as CreateBasketCardValues;
-
-    const findFirstBasket = await prisma.cartProduct.findFirst({
-      where: {
-        cartId: userBasket.id,
-        variantId: data.variantId,
-        ingredients: {
-          every: {
-            id: { in: data.ingredients },
-          },
-        },
-      },
-    });
-
-    if (findFirstBasket) {
-      await prisma.cartProduct.update({
-        where: {
-          id: findFirstBasket.id,
-        },
-        data: {
-          quantity: findFirstBasket.quantity + 1,
-        },
-      });
-    } else {
-      await prisma.cartProduct.create({
-        data: {
-          cartId: userBasket.id,
-          variantId: data.variantId,
-          quantity: 1,
-          ingredients: { connect: data.ingredients?.map((id) => ({ id })) },
-        },
-      });
-    }
-
-    const updateUserBasket = updateBasketTotalAmount(token);
-    const response = NextResponse.json(updateUserBasket);
-    response.cookies.set("basketToken", token);
-    return response;
-  } catch (error) {
-    console.log("POST_CATCH ERROR", error);
-    return NextResponse.json({ error: "basket not create" });
   }
 }
